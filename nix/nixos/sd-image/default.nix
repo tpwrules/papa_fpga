e67ae2d55c31e6935e63c27679aa1f60bedc48d9
Thomas Watson <twatson52@icloud.com>
2023-08-26 23:23:24 -0400

    sd-image: add stuff to be able to log in

--- nix/nixos/sd-image/default.nix
+++ nix/nixos/sd-image/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     (modulesPath + "/profiles/minimal.nix")
-    (modulesPath + "/installer/cd-dvd/sd-image.nix")
+    (modulesPath + "/installer/sd-card/sd-image.nix")
   ];
 
   boot.loader.generic-extlinux-compatible.enable = true;
@@ -26,7 +26,32 @@
   sdImage.populateFirmwareCommands = ''
   '';
 
-  environment.systemPackages = lib.mkForce [];
+  # Use less privileged nixos user
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+    # Allow the graphical user to login without password
+    initialHashedPassword = "";
+  };
+
+  # Allow the user to log in as root without a password.
+  users.users.root.initialHashedPassword = "";
+
+  # Allow passwordless sudo from nixos user
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  # Automatically log in at the virtual consoles.
+  services.getty.autologinUser = "nixos";
+
+  environment.systemPackages = with pkgs; lib.mkForce [
+    bash
+    nano
+    nix
+    coreutils
+  ];
 
   # save space and compilation time. might revise?
   hardware.enableAllFirmware = lib.mkForce false;
