280dfd7c954376e0cbcd289e53eba1f3a139f3db
Thomas Watson <twatson52@icloud.com>
2023-11-28 11:08:22 -0600

    disable fake mics for now

--- design/amaranth_top/amaranth_top/constants.py
+++ design/amaranth_top/amaranth_top/constants.py
@@ -4,7 +4,7 @@ from amaranth import *
 
 MIC_FREQ_HZ = 48000
 CAP_DATA_BITS = 16 # lowest 8 mic data bits thrown away
-USE_FAKE_MICS = True
+USE_FAKE_MICS = False
 
 # total number of microphones to take input from (must be even)
 # right mics are even (0, 2, 4, ...), left mics are odd (1, 3, 5, ...)
