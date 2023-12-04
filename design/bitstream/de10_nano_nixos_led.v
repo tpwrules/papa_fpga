3e3790988e5829f0c94c893c788b322813fcd046
Thomas Watson <twatson52@icloud.com>
2023-12-03 19:14:04 -0600

    delete seemingly useless SDRAM stuff

--- design/bitstream/de10_nano_nixos_led.v
+++ design/bitstream/de10_nano_nixos_led.v
@@ -34,24 +34,6 @@ module de10_nano_nixos_led(
 	input 		          		HDMI_TX_INT,
 	output		          		HDMI_TX_VS,
 
-	//////////// HPS //////////
-	output		    [14:0]		HPS_DDR3_ADDR,
-	output		     [2:0]		HPS_DDR3_BA,
-	output		          		HPS_DDR3_CAS_N,
-	output		          		HPS_DDR3_CKE,
-	output		          		HPS_DDR3_CK_N,
-	output		          		HPS_DDR3_CK_P,
-	output		          		HPS_DDR3_CS_N,
-	output		     [3:0]		HPS_DDR3_DM,
-	inout 		    [31:0]		HPS_DDR3_DQ,
-	inout 		     [3:0]		HPS_DDR3_DQS_N,
-	inout 		     [3:0]		HPS_DDR3_DQS_P,
-	output		          		HPS_DDR3_ODT,
-	output		          		HPS_DDR3_RAS_N,
-	output		          		HPS_DDR3_RESET_N,
-	input 		          		HPS_DDR3_RZQ,
-	output		          		HPS_DDR3_WE_N,
-
 	//////////// KEY //////////
 	input 		     [1:0]		KEY,
 
@@ -346,23 +328,6 @@ cyclonev_hps_interface_fpga2sdram f2sdram(
 soc_system u0(
     .clk_clk(FPGA_CLK1_50),
     .reset_reset_n(hps_fpga_reset_n),
-
-    .memory_mem_a(HPS_DDR3_ADDR),
-    .memory_mem_ba(HPS_DDR3_BA),
-    .memory_mem_ck(HPS_DDR3_CK_P),
-    .memory_mem_ck_n(HPS_DDR3_CK_N),
-    .memory_mem_cke(HPS_DDR3_CKE),
-    .memory_mem_cs_n(HPS_DDR3_CS_N),
-    .memory_mem_ras_n(HPS_DDR3_RAS_N),
-    .memory_mem_cas_n(HPS_DDR3_CAS_N),
-    .memory_mem_we_n(HPS_DDR3_WE_N),
-    .memory_mem_reset_n(HPS_DDR3_RESET_N),
-    .memory_mem_dq(HPS_DDR3_DQ),
-    .memory_mem_dqs(HPS_DDR3_DQS_P),
-    .memory_mem_dqs_n(HPS_DDR3_DQS_N),
-    .memory_mem_odt(HPS_DDR3_ODT),
-    .memory_mem_dm(HPS_DDR3_DM),
-    .memory_oct_rzqin(HPS_DDR3_RZQ),
 
     .h2f_lw_awid(h2f_lw_awid),
     .h2f_lw_awaddr(h2f_lw_awaddr),
